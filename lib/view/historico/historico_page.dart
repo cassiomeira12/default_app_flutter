2363ef77954582328e6c7e80849f185449fe0dbb
Cássio Meira Silva <cassiomeira_12@hotmail.com>
2020-03-29 19:11:40 -0300

    Correções

--- lib/view/historico/historico_page.dart
+++ lib/view/historico/historico_page.dart
@@ -23,33 +23,33 @@ class _HistoricoPageState extends State<HistoricoPage> {
       body: SingleChildScrollView(
         child: Stack(
           children: <Widget>[
-//            BackgroundCard(),
-//            SingleChildScrollView(
-//              child: ShapeRound(
-//                  _showForm()
-//              ),
-//            ),
+            BackgroundCard(),
+            SingleChildScrollView(
+              child: ShapeRound(
+                  _showForm()
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-//  Widget _showForm() {
-//    return new Container(
-//      padding: EdgeInsets.all(12.0),
-//      child: new Form(
-//        key: _formKey,
-//        child: Column(
-//          children: <Widget>[
-//            //textTitle(),
-//            //emailInput(),
-//            //textMensagem(),
-//            //_isLoading ? showCircularProgress() : sendButton()
-//          ],
-//        ),
-//      ),
-//    );
-//  }
+  Widget _showForm() {
+    return new Container(
+      padding: EdgeInsets.all(12.0),
+      child: new Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            //textTitle(),
+            //emailInput(),
+            //textMensagem(),
+            //_isLoading ? showCircularProgress() : sendButton()
+          ],
+        ),
+      ),
+    );
+  }
 
 }
